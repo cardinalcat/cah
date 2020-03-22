9043e99e831379f4488db54fe0ccbdf1440e879d
julian lazaras <lazaras@pdx.edu>
2020-03-21 18:25:43 -0700

    black cards added

--- cah-backend/src/main.rs
+++ cah-backend/src/main.rs
@@ -1,18 +1,26 @@
 extern crate ws;
+extern crate rand;
+use rand::Rng;
 
 use ws::listen;
 
 fn main() {
-
     // Listen on an address and call the closure for each connection
     if let Err(error) = listen("127.0.0.1:3012", |out| {
         // The handler needs to take ownership of out, so we use move
-        move |msg| {
+        move |msg: ws::Message| {
             // Handle messages received on this connection
             println!("Server got message '{}'. ", msg);
-
+            let content = msg.clone();
+            if msg.into_text().unwrap() == "start".to_string(){
+                let mut rng = rand::thread_rng();
+                let hash: u16 = rng.gen();
+                out.send(format!("{}", hash));
+            }else{
             // Use the out channel to send messages back
-            out.send(msg)
+                out.send(format!("unknown command {}", content));
+            }
+            Ok(())
         }
     }) {
         // Inform the user of failure
