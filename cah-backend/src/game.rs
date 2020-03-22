b00a529ad13cd86993a3a67044154c282255c64f
julian lazaras <lazaras@pdx.edu>
2020-03-21 22:15:34 -0700

    perfection

---
+++ cah-backend/src/game.rs
@@ -0,0 +1,101 @@
+use rand::Rng;
+use std::fs::File;
+use std::io::Read;
+use ws::WebSocket;
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub enum Color {
+    Black,
+    White,
+}
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub enum Kind {
+    One,
+    Two,
+    Three,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub struct Card {
+    id: u16,
+    kind: Kind,
+    color: Color,
+    text: String,
+}
+impl Card {
+    pub fn get_text(&self) -> String {
+        self.text.clone()
+    }
+}
+pub struct User {
+    white_cards: Vec<Card>,
+    black_cards: Vec<Card>,
+}
+pub struct Game {
+    users: Vec<User>,
+    draw_white: Vec<Card>,
+    draw_black: Vec<Card>,
+    discard: Vec<Card>,
+    hash: u16,
+}
+impl Game {
+    pub fn new(hash: u16) -> Self {
+        let mut wcontents = String::new();
+        let mut bcontents = String::new();
+        File::open("black-cards.json")
+            .expect("no black cards")
+            .read_to_string(&mut bcontents);
+        File::open("white-cards.json")
+            .expect("no white cards")
+            .read_to_string(&mut wcontents);
+        let mut draw_white: Vec<Card> = Vec::new();
+        let mut draw_black: Vec<Card> = Vec::new();
+        for card in bcontents.split("|") {
+            let card = card.trim();
+            if !card.is_empty() {
+                draw_black.push(serde_json::from_str(card).unwrap());
+            }
+        }
+        for card in wcontents.split("|") {
+            let card = card.trim();
+            if !card.is_empty() {
+                draw_white.push(serde_json::from_str(card).unwrap());
+            }
+        }
+        println!("draw_black: {}", draw_black.len());
+        Game {
+            users: Vec::new(),
+            draw_black,
+            draw_white,
+            discard: Vec::new(),
+            hash,
+        }
+    }
+    pub fn get_discard(&self) -> Vec<Card> {
+        self.discard.clone()
+    }
+    pub fn get_all_white(&self) -> Vec<Card> {
+        self.draw_white.clone()
+    }
+    pub fn draw_white(&mut self) -> Card {
+        if self.draw_white.is_empty() {
+            println!("empty");
+            self.draw_white.append(&mut self.discard);
+            self.discard.clear();
+        }
+        println!("draw_white: {}", self.draw_white.len());
+        let mut rng = rand::thread_rng();
+        let hash: usize = rng.gen::<usize>() % self.draw_white.len();
+        self.draw_white.remove(hash)
+    }
+    pub fn draw_black(&mut self) -> Card {
+        let mut rng = rand::thread_rng();
+        let hash: usize = rng.gen::<usize>() % self.draw_black.len();
+        self.draw_black.remove(hash)
+    }
+    pub fn count_black(&self) -> usize {
+        self.draw_black.len()
+    }
+    pub fn count_white(&self) -> usize {
+        self.draw_white.len()
+    }
+}
--- cah-backend/src/main.rs
+++ cah-backend/src/main.rs
@@ -1,23 +1,35 @@
-extern crate ws;
 extern crate rand;
+extern crate ws;
+use cah_backend::game::Game;
 use rand::Rng;
-
 use ws::listen;
 
 fn main() {
-    // Listen on an address and call the closure for each connection
-    if let Err(error) = listen("127.0.0.1:3012", |out| {
+    let mut rng = rand::thread_rng();
+    let hash: u16 = rng.gen();
+    let mut game = Game::new(hash);
+    let mut index: usize = 0;
+    let black_card_count = game.count_black();
+    while (index < black_card_count) {
+        println!(
+            "black: {}\n\n, white: {}\n",
+            game.draw_black().get_text(),
+            game.draw_white().get_text(),
+        );
+        println!("{}", index);
+        index = index + 1;
+    }
+    /*if let Err(error) = listen("127.0.0.1:3012", |out| {
         // The handler needs to take ownership of out, so we use move
         move |msg: ws::Message| {
             // Handle messages received on this connection
             println!("Server got message '{}'. ", msg);
             let content = msg.clone();
-            if msg.into_text().unwrap() == "start".to_string(){
-                let mut rng = rand::thread_rng();
-                let hash: u16 = rng.gen();
+            if msg.into_text().unwrap() == "start".to_string() {
+
                 out.send(format!("{}", hash));
-            }else{
-            // Use the out channel to send messages back
+            } else {
+                // Use the out channel to send messages back
                 out.send(format!("unknown command {}", content));
             }
             Ok(())
@@ -25,5 +37,5 @@ fn main() {
     }) {
         // Inform the user of failure
         println!("Failed to create WebSocket due to {:?}", error);
-    }
+    }*/
 }
