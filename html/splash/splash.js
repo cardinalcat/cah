2f1cdcfb258eb54e68490ce0e434fabc04106756
jrbobdobbs <gleason.ariel@gmail.com>
2021-03-28 11:57:36 -0700

    Basic JS button functionality achieved. Basic CSS styles as well.

--- html/splash/splash.js
+++ html/splash/splash.js
@@ -1,14 +1,4 @@
-var modal = document.getElementById("Rules");
-var btn = document.getElementById("open-rules");
-var span = document.getElementsByClassName("close)[0];
-
-btn.onClick = function() {
-    alert("Button clicked");
-    modal.style.display = "block";
-}
-
-window.onclick = function(event) {
-    if(event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+    var rulesButton = document.getElementById("rules");
+    rulesButton.addEventListener("click", function() {
+        window.alert("Button clicked!");
+    });
